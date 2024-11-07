d1484fe6cb21ec642fa096cfabff58daa110597c
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2024-11-07 14:01:39 +0200

    Update settings.py

--- server/toota/settings.py
+++ server/toota/settings.py
@@ -26,12 +26,12 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = '@sgjkndrdxjkixsgjddckhvfjkjdkf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
