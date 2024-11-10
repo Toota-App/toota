62511430fb3cf0b6364871cc73e049557add1057
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2024-11-10 07:55:34 +0200

    Update settings.py

--- server/toota/settings.py
+++ server/toota/settings.py
@@ -126,7 +126,7 @@ CORS_ALLOW_CREDENTIALS = True
 #     }
 # }
 
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
