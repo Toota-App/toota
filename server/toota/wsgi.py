5df5e324264c28e55a78ded154cbc16a59e654a8
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2024-11-10 08:28:21 +0200

    Update wsgi.py

--- server/toota/wsgi.py
+++ server/toota/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#settings_module = 'toota.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'toota.settings'
+settings_module = 'toota.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'toota.settings'
 
-settings_module =  'toota.settings'
+#settings_module =  'toota.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
