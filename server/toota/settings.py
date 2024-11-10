18d700874a9fcca00dc97bd801d1f6521398d14c
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2024-11-10 08:08:55 +0200

    Update settings.py

--- server/toota/settings.py
+++ server/toota/settings.py
@@ -126,15 +126,14 @@ CORS_ALLOW_CREDENTIALS = True
 #     }
 # }
 
-conn_str = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': os.getenv(”DATABASE”),
+        'HOST': os.getenv(”SERVER”),
+        'USER': os.getenv(”USER”),
+        'PASSWORD': os.getenv(”PASSWORD”),
     }
 }
 
