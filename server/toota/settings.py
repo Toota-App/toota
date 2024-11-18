3495e761e82aeaf181cbec5b8442208ff8754ec6
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2024-11-18 19:29:02 +0200

    Update settings.py

--- server/toota/settings.py
+++ server/toota/settings.py
@@ -17,13 +17,9 @@ environ.Env.read_env()
 SECRET_KEY = '@sgjkndrdxjkixsgjddckhvfjkjdkf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    'https://toota-gwgmcdefdqhde3g6.southafricanorth-01.azurewebsites.net',
-    'auth.toota.co.za',
-    '*'
-]
+ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
