d51a30c02d3da7bb0d67e41765f358c3c140e83a
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2024-11-19 17:01:54 +0200

    Update deployment.py

--- server/toota/deployment.py
+++ server/toota/deployment.py
@@ -69,12 +69,3 @@ LOGGING = {
 
 
 STATIC_ROOT = BASE_DIR/'staticfiles'
-# Email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.tootapp.co.za'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
