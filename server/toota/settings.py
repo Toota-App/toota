fabd04766e0c3d2eb8c9bf807bbdebdb7f528818
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2024-11-21 10:35:02 +0200

    Update settings.py with adding cors

--- server/toota/settings.py
+++ server/toota/settings.py
@@ -79,7 +79,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'https://localhost:5173',
     'http://localhost:8000',
-]
+    'https://toota-gwgmcdefdqhde3g6.southafricanorth-01.azurewebsites.net',
+    'https://nice-island-045956903.5.azurestaticapps.net',
+    
 
 CORS_ALLOW_METHODS = [
     "DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"
@@ -88,6 +90,9 @@ CORS_ALLOW_METHODS = [
 CSRF_TRUSTED_ORIGINS = [
     'https://localhost:5173',
     'http://localhost:8000',
+    'https://toota-gwgmcdefdqhde3g6.southafricanorth-01.azurewebsites.net',
+    'https://nice-island-045956903.5.azurestaticapps.net',
+    
 ]
 
 CORS_ALLOW_CREDENTIALS = True
