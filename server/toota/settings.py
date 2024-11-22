54d1827f56d315ca2fd908c14e4bd0d3017b7699
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2024-11-22 16:08:50 +0200

    Update settings.py

--- server/toota/settings.py
+++ server/toota/settings.py
@@ -79,6 +79,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'https://localhost:5173',
     'http://localhost:8000',
+    'http://tootapp.co.za',
     'https://toota-gwgmcdefdqhde3g6.southafricanorth-01.azurewebsites.net',
     'https://nice-island-045956903.5.azurestaticapps.net',
 ]
